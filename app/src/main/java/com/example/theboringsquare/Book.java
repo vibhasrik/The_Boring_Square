894e4da7ca389ea1184d6932e83e4825dc073868
s145000 <vibhasrik@gmail.com>
2023-04-24 11:14:36 -0500

    Book and DBManager Classes Updated

--- app/src/main/java/com/example/theboringsquare/Book.java
+++ app/src/main/java/com/example/theboringsquare/Book.java
@@ -1,4 +1,91 @@
 package com.example.theboringsquare;
 
 public class Book {
+    private int id;
+    private String name;
+    private String authorFirst;
+    private String authorLast;
+    private String genre;
+    private String img;
+    private String description;
+    private double price;
+
+    public Book( int newId, String newName, double newPrice ) {
+        setId( newId );
+        setName( newName );
+        setPrice( newPrice );
+    }
+
+    // S E T T E R   M E T H O D S
+    public void setId( int newId ) {
+        id = newId;
+    }
+
+    public void setName( String newName ) {
+        name = newName;
+    }
+    public void setPrice( double newPrice ) {
+        if( newPrice >= 0.0 )
+            price = newPrice;
+    }
+
+    public void setGenre( String newGenre ) {
+        genre = newGenre;
+    }
+
+    public void setAuthorFirst( String newAuthorFirst ) {
+        authorFirst = newAuthorFirst;
+    }
+
+    public void setAuthorLast( String newAuthorLast ) {
+        authorLast = newAuthorLast;
+    }
+
+    public void setImg( String newImg ) {
+        img = newImg;
+    }
+
+    public void setDescription( String newDescription ) {
+        description = newDescription;
+    }
+
+    // G E T T E R   M E T H O D S
+
+    public int getId( ) {
+        return id;
+    }
+
+    public String getName( ) {
+        return name;
+    }
+
+    public double getPrice( ) {
+        return price;
+    }
+
+    public String getGenre( ) {
+        return genre;
+    }
+
+    public String getImg( ) {
+        return img;
+    }
+
+    public String getDescription( ) {
+        return description;
+    }
+
+    public String getAuthorFirst( ) {
+        return authorFirst;
+    }
+
+    public String getAuthorLast( ) {
+        return authorLast;
+    }
+
+    public String toString( ) {
+        return id + "; " + name + "; " + price;
+    }
 }
+
+
--- app/src/main/java/com/example/theboringsquare/DatabaseManager.java
+++ app/src/main/java/com/example/theboringsquare/DatabaseManager.java
@@ -1,4 +1,123 @@
 package com.example.theboringsquare;
 
-public class DatabaseManager {
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class DatabaseManager extends SQLiteOpenHelper
+{
+    private static final String DATABASE_NAME = "bookDB";
+    private static final int DATABASE_VERSION = 1;
+    
+    private static final String TABLE_BOOK = "book";
+    private static final String ID = "id";
+    private static final String NAME = "name";
+    private static final String GENRE = "genre";
+    private static final String DESCRIPTION = "description";
+    private static final String AUTHORFIRST = "authorFirst";
+    private static final String AUTHORLAST = "authorLast";
+    private static final String IMG = "img";
+    private static final String PRICE = "price";
+
+    public DatabaseManager( Context context ) {
+        super( context, DATABASE_NAME, null, DATABASE_VERSION );
+    }
+
+    public void onCreate( SQLiteDatabase db ) {
+        // build sql create statement
+        String sqlCreate = "create table " + TABLE_BOOK + "( " + ID;
+        sqlCreate += " integer primary key autoincrement, " + NAME;
+        sqlCreate += " text, " + PRICE + " real )";
+        db.execSQL(sqlCreate);
+    }
+
+    public void onUpgrade( SQLiteDatabase db, int oldVersion, int newVersion ) {
+        // Drop old table if it exists
+        db.execSQL( "drop table if exists " + TABLE_BOOK );
+        // Re-create tables
+        onCreate( db );
+    }
+
+    // Insert book into database
+    public void insert( Book book ) {
+        SQLiteDatabase db = this.getWritableDatabase( );
+        String sqlInsert = "insert into " + TABLE_BOOK;
+        sqlInsert += " values( null, '" + book.getName( );
+        sqlInsert += "', '" + book.getPrice( ) + "' )";
+        db.execSQL( sqlInsert );
+        db.close( );
+    }
+
+    // Delete book from database
+    public void deleteById( int id ) {
+        SQLiteDatabase db = this.getWritableDatabase( );
+        String sqlDelete = "delete from " + TABLE_BOOK;
+        sqlDelete += " where " + ID + " = " + id;
+        Log.w("DatabaseManager", sqlDelete);
+        db.execSQL( sqlDelete );
+        db.close();
+    }
+
+    //point size = new point();
+    //getWindowManager().getDefaultDisplay().getSize(size);
+    //buttonWidth = size/7;
+
+    // Update book record
+    public void updateById( int id, String name, double price, String genre, String authorFirst, String authorLast, String description, String img ) {
+        SQLiteDatabase db = this.getWritableDatabase( );
+        String sqlUpdate = "update " + TABLE_BOOK;
+        sqlUpdate += " set " + NAME + " = \"" + name + "\", " +
+                PRICE + " = " + price +
+                GENRE + " = \"" + genre + "\", " +
+                AUTHORFIRST + " = \"" + authorFirst + "\", " +
+                AUTHORLAST + " = \"" + authorLast + "\", " +
+                DESCRIPTION + " = \"" + description + "\", " +
+                IMG + " = \"" + img + "\", " +
+                " where " +
+                ID + " = " + id;
+        db.execSQL( sqlUpdate );
+//        sqlUpdate = "update " + TABLE_BOOK;
+//        sqlUpdate += " set " + PRICE + " = " + price + " where " + ID + " = " + id;
+//        db.execSQL( sqlUpdate );
+        db.close();
+    }
+
+
+    // Return list of all candies
+    public ArrayList<Book> selectAll( ) {
+        ArrayList<Book> candies = new ArrayList<Book>();
+        SQLiteDatabase db = this.getWritableDatabase( );
+        String sqlQuery = "select * from " + TABLE_BOOK;
+        Cursor cursor = db.rawQuery( sqlQuery, null );
+        while( cursor.moveToNext( ) ) {
+            Book currentBook = new Book( Integer.parseInt(
+                    cursor.getString( 0 ) ),
+                    cursor.getString( 1 ), cursor.getDouble( 2 ) );
+            candies.add( currentBook );
+        }
+        db.close( );
+
+        return candies;
+    }
+
+    // Select book by id
+    public Book selectById( int id ) {
+        SQLiteDatabase db = this.getWritableDatabase( );
+        String sqlQuery = "select * from " + TABLE_BOOK;
+        sqlQuery += " where " + ID + " = " + id;
+        Cursor cursor = db.rawQuery( sqlQuery, null );
+        Book book = null;
+        if( cursor.moveToFirst( ) )
+            book = new Book(
+                    Integer.parseInt( cursor.getString( 0 ) ),
+                    cursor.getString( 1 ),
+                    cursor.getDouble( 2 ) );
+        return book;
+    }
+
 }
+
